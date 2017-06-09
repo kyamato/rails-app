4dadfbb095b1d269dc9977be07ef586026e77575
kyamato <kyamato@idcf.jp>
2017-06-09 18:22:07 +0900

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  root 'welcome#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'posts#index'
 end
